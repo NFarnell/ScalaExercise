e1cc74a1ba8aae0558b0f40ddd1b955714f973b0
NFarnell <Farnell95@googlemail.com>
2020-03-03 12:11:43 +0000

    Day2 Updated FizzBuzz

--- src/main/scala/ExercisesDay2/FizzBuzz.scala
+++ src/main/scala/ExercisesDay2/FizzBuzz.scala
@@ -4,19 +4,29 @@ object FizzBuzz extends App {
   def FizzyBuzz(word: String, word2: String, num: Int): Unit = {
 var i = 0
 var Output = ""
-    while(i < num)
-      i +=1
-      if(i % 3 == 0){
-        Output += word
+    var Output2 = ""
+    for(i <- 1 to  num) {
+
+
+      if (i % 3 == 0) {
+        Output += word + ","
       }
-      if(i % 5 == 0){
-        Output += word2
+      if (i % 5 == 0) {
+        Output += word2 + ","
+      }
+      if (i % 3 != 0 && i % 5 != 0) {
+        Output += i+","
+
       }
 
+    }
+
+
     println(Output)
+
 
   }
 
 
-  FizzyBuzz("Fizz", "Buzz", 5)
+  FizzyBuzz("Fizz", "Buzz", 15)
 }
---
+++ src/main/scala/ExercisesDay2/PatternMatching.scala
@@ -0,0 +1,11 @@
+package ExercisesDay2
+
+object PatternMatching extends App {
+  def PatternMatchin(num:Int,num2:Int): Unit ={
+    val array = Array(num, num2)
+    println(array.last)
+    println(array.head)
+  }
+
+PatternMatchin(5,10)
+}
